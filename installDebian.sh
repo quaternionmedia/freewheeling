e9ee19e76932d1e45a67226fd987b92941fb55a5
Harpo <harpo@harpo.me>
2020-12-17 22:53:00 -0800

    fixed install script to make runable

--- installDebian.sh
+++ installDebian.sh
@@ -2,12 +2,7 @@
 
 # install script for freewheeling on debian
 
-if [ "$EUID" -ne 0 ]; then
-	echo "This script must be run as sudo. Please try again"
-	exit 1
-fi
-
-if [[ $(cat /etc/issue) =~ "Linux Mint 18" ]]; then
+if [[ $(cat /etc/issue) =~ "Linux Mint" ]]; then
 	echo "installing freewheeling on linux mint"
 
 	DEPS="libgnutls-dev libfreetype6-dev libsdl-gfx1.2-dev libsdl-ttf2.0-dev liblo-dev libxml2-dev libvorbis-dev libsndfile1-dev libjack-jackd2-dev libfluidsynth2-devel autoconf automake nettle-dev"
@@ -16,6 +11,7 @@ if [[ $(cat /etc/issue) =~ "Linux Mint 18" ]]; then
 		sudo apt-get install -y $i
 	done
 
+	aclocal &&\
 	./configure &&\
 	autoconf &&\
 	automake &&\
